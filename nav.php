f3156a443a82fbda1d905391289f3e73e6ef5bd8
Jeancarlosayala <jeancarlosayala57@gmail.com>
2020-05-15 18:44:40 -0500

    v1.0

--- nav.php
+++ nav.php
@@ -13,9 +13,13 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php">Logo</a>
 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="col col-2">
+            <div class="card-body">
+                <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="" class="card-img-top" id="logo"></a>
+            </div>
+        </div>
         <form class="form-inline ml-auto" action="views/buscador.php" method="POST">
             <input class="form-control mr-sm-2" type="text" name="buscar" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
